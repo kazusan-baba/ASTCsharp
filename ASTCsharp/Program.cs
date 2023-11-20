ecb950f01125c3eb8b2480827f472c03bf24fad7
kazusan-baba <Desktop>
2023-11-20 13:01:20 +0900

    BlockSyntaxレベルでの置き換え

--- ASTCsharp/Program.cs
+++ ASTCsharp/Program.cs
@@ -22,25 +22,37 @@ class Program
 SyntaxTree tree = CSharpSyntaxTree.ParseText(code);
 
 // ルートノードの取得
-var root = (CompilationUnitSyntax)tree.GetRoot();
+var root = tree.GetCompilationUnitRoot();
+var classSyntax = root.ChildNodes().OfType<ClassDeclarationSyntax>().First();
+var methodSyntaxes = classSyntax.ChildNodes();
+var blockSyntax = methodSyntaxes.ElementAt(1).ChildNodes().OfType<BlockSyntax>().First();
 
 // 新しいメソッド呼び出しノードを作成
-var newStatement = SyntaxFactory.ExpressionStatement(
-    SyntaxFactory.InvocationExpression(
-        SyntaxFactory.MemberAccessExpression(
-            SyntaxKind.SimpleMemberAccessExpression,
-            SyntaxFactory.IdentifierName("Console"),
-            SyntaxFactory.IdentifierName("WriteLine")),
-        SyntaxFactory.ArgumentList(
-            SyntaxFactory.SingletonSeparatedList(
-                SyntaxFactory.Argument(
-                    SyntaxFactory.LiteralExpression(
-                        SyntaxKind.StringLiteralExpression,
-                        SyntaxFactory.Literal("Modified code")))))));
+var newStatement = SyntaxFactory.Block(
+    SyntaxFactory.ExpressionStatement(
+        SyntaxFactory.InvocationExpression(
+            SyntaxFactory.MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                SyntaxFactory.IdentifierName("Console"),
+                SyntaxFactory.IdentifierName("WriteLine")),
+            SyntaxFactory.ArgumentList(
+                SyntaxFactory.SingletonSeparatedList(
+                    SyntaxFactory.Argument(
+                        SyntaxFactory.LiteralExpression(
+                            SyntaxKind.StringLiteralExpression,
+                            SyntaxFactory.Literal("Modified code")
+                        )
+                    )
+                )
+            )
+        )
+    )
+);
 
 
 // 既存のステートメントを新しいステートメントで置き換える
-var newRoot = root.ReplaceNode(root.DescendantNodes().OfType<ExpressionStatementSyntax>().First(), newStatement);
+var newRoot = root.ReplaceNode(blockSyntax, newStatement);
+
 
 // 変更を適用したコードを取得
 var newCode = newRoot.ToFullString();
